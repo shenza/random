5235faa0ea2d7293fe22dfb7abe87caec58726cf
shenza <krishanmunthree@gmail.com>
2020-02-28 00:04:14 +1100

    readme file

--- api_test/urls.py
+++ api_test/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api2/', include('api_test.apps.bankai.urls')),
     
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    #re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     #re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),  
 ]
